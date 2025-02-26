5c20027f3d9989a0de73f21aa1db9f91a6b75eeb
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-02-26 19:51:47 +0000

    Deleted UserAuth

--- app001/app001/urls.py
+++ app001/app001/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/", include("userauth.urls")),
+    
     path("", include("core.urls")),
 ]
 
