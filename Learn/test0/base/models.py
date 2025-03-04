11b34609091666023be30e810d36aefd4428b467
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-03-05 02:16:15 +0600

    Created fake recipe. creation py file in tests.py

--- Learn/test0/base/models.py
+++ Learn/test0/base/models.py
@@ -1,18 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-
-class Recipe(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200, default="Chicken Curry")
-    description = models.TextField(default="This is a chicken curry recipe.")
-    
-    day = models.DateField(auto_now_add=True)
-    
-    def __str__(self):
-        return f"{self.title} - {self.user.username}"
-    
-    class Meta:
-        verbose_name = "Recipe"
-        verbose_name_plural = "Recipes"
--- Learn/test0/recipe/tests.py
+++ Learn/test0/recipe/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from faker import Faker
+from django.contrib.auth.models import User
+from .models import Recipe
+import random
 
 # Create your tests here.
+
+fake = Faker()
+
+def fake_recipe():
+    if not User.objects.exists():
+        for i in range(10):
+            user = User.objects.create_user(username=fake.name(), email=fake.email(), password="password")
+            user.save()
+    users = User.objects.all()
+    
+    for i in range(10):
+        user = random.choice(users)
+        recipe = Recipe.objects.create(user=user, title=fake.sentence(), description=fake.text())
+        recipe.save()
+    
+    print("Recipes created")
+    
+    if __name__ == "__main__":
+        fake_recipe()
