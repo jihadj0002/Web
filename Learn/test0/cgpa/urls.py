16eb2a49b38bd308e1210c8419bad833ed67588e
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-03-07 03:19:53 +0600

    Designing and university adding left

--- Learn/test0/cgpa/urls.py
+++ Learn/test0/cgpa/urls.py
@@ -5,4 +5,7 @@ app_name = "cgpa"
 
 urlpatterns = [
     path("", views.index, name="index"),
-]
+    path("result/", views.result, name="result"),
+    path("edit/<int:pk>", views.edit_sub, name="edit_sub"),
+    path("del/<int:pk>", views.delet, name="delete"),
+] 
