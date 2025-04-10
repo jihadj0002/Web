4c30718921f870912f3450ce0363f60eda8d1653
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-04-11 02:17:00 +0600

    Created user model in userauth

--- Learn/test1/userauth/models.py
+++ Learn/test1/userauth/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser, Group
+
+import uuid
 
 # Create your models here.
+
+class User(AbstractUser):
+    # Custom fields
+    USER_TYPE_CHOICES = (
+        ('customer', 'Customer'),
+        ('vendor', 'Vendor'),
+        ('admin', 'Admin'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='customer')
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='user_profiles/', blank=True, null=True)
+    email_verified = models.BooleanField(default=False)
+    verification_token = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    # Fix for groups conflict
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name="core_user_set",  # Changed
+        related_query_name="core_user",
+    )
+    
+    def __str__(self):
+        return self.username
+    
